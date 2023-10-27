eee875ae8d3a2c4522b023894a25b9bff7ae2045
marcocentonze <marco.centonze27@gmail.com>
2023-10-27 17:39:19 +0200

    ticket

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,24 @@
+<footer class="bg-dark text-light">
+    <div class="container py-4">
+        <div class="row">
+            <div class="col-md-4">
+                <h4>Informazioni</h4>
+                <p>Il nostro sito web dedicato al mondo del cinema.</p>
+            </div>
+            <div class="col-md-4">
+                <h4>Link utili</h4>
+                <ul class="list-unstyled">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/film">Film</a></li>
+                    <li><a href="/contatti">Contatti</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h4>Seguici su</h4>
+                <a href="#" class="text-light"><i class="fab fa-facebook fa-2x"></i></a>
+                <a href="#" class="text-light"><i class="fab fa-twitter fa-2x"></i></a>
+                <a href="#" class="text-light"><i class="fab fa-instagram fa-2x"></i></a>
+            </div>
+        </div>
+    </div>
+</footer>
